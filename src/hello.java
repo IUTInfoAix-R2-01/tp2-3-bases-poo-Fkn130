bb87b4bce41d282f92a5bf01f44820e504d9d453
Furkan <furkan.gul@etu.univ-amu.fr>
2025-01-31 16:38:28 +0100

    je traduis en espagnol

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,6 @@
-public class hello {   // Save as "Hello.java" under "d:\myProject"
+public class hello {   
    public static void main(String[] args) {
       System.out.println("Hello, world!");
+      System.out.println("Helloworld en espagnol");
    }
 }
